d25cc8caed3492aaf4d6cf2091243ea83d8ba698
Douglas Dennys <douglasdennys45@gmail.com>
2023-04-13 20:40:08 -0300

    ajustando codigo

--- response_test.go
+++ response_test.go
@@ -2,18 +2,16 @@ package goresponse_test
 
 import (
 	"testing"
-	"time"
 
 	goresponse "github.com/douglasdennys45/go-response"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestResponse(t *testing.T) {
-	r := goresponse.NewResponse("test", "test", "test", 200, "test", "test", time.Now())
+	r := goresponse.NewResponse("test", "test", "test", 200, "test", "test")
 	response := r.Response()
 	assert.Equal(t, response.Data, r.Response().Data)
 	assert.Equal(t, response.Error, r.Response().Error)
-	assert.Equal(t, response.Timestamp, r.Response().Timestamp)
 	assert.Equal(t, response.Message, r.Response().Message)
 	assert.Equal(t, response.Path, r.Response().Path)
 	assert.Equal(t, response.RequestId, r.Response().RequestId)
